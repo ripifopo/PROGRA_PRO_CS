7ab17ffb540a8f23c3004a127692c9118be0f43e
Tomás Poblete R <177884212+tomaspobb@users.noreply.github.com>
2025-03-19 10:40:51 -0300

    .

--- main/components/Navbar.js
+++ main/components/Navbar.js
@@ -1,15 +1,1 @@
-import React from 'react';
 
-const Navbar = () => {
-  return (
-    <nav style={{ backgroundColor: '#333', padding: '10px' }}>
-      <div style={{ maxWidth: '1200px', margin: '0 auto', color: '#fff' }}>
-        <a href="/" style={{ color: '#fff', marginRight: '20px', textDecoration: 'none' }}>Inicio</a>
-        <a href="#menu" style={{ color: '#fff', marginRight: '20px', textDecoration: 'none' }}>Menú</a>
-        <a href="#reservas" style={{ color: '#fff', textDecoration: 'none' }}>Reservas</a>
-      </div>
-    </nav>
-  );
-};
-
-export default Navbar;
