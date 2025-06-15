eb6c1d7423cd2aa794d186f4aa6cdd00aea7521f
Tomás Poblete R <177884212+tomaspobb@users.noreply.github.com>
2025-06-14 20:40:01 -0400

    Configuración Vercel

--- MediSearch/next.config.ts
+++ MediSearch/next.config.ts
@@ -1,6 +1,20 @@
-const nextConfig = {
+import path from 'path';
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     domains: ['beta.cruzverde.cl', 'www.farmaciasahumada.cl', 'www.salcobrand.cl'],
   },
+  webpack: (config) => {
+    config.resolve = {
+      ...(config.resolve || {}),
+      alias: {
+        ...(config.resolve?.alias || {}),
+        '@': path.resolve(__dirname, 'src'),
+      },
+    };
+    return config;
+  },
 };
+
 export default nextConfig;
