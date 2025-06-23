71136f67d8c4d4272be222531118c6b48aab5c8a
Tomás Poblete R <177884212+tomaspobb@users.noreply.github.com>
2025-06-23 00:04:40 -0400

    .

--- MediSearch/jest.config.js
+++ MediSearch/jest.config.js
@@ -2,12 +2,17 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.jest.json',
+    },
+  },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   testMatch: ['**/tests/**/*.test.ts?(x)'],
 };
