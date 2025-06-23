749e318af606d716b7c9e23bd2fc47e711a3f19e
Tomás Poblete R <177884212+tomaspobb@users.noreply.github.com>
2025-06-22 23:52:55 -0400

    Arreglo Vercel

--- MediSearch/src/app/api/zones/[pharmacy]/route.ts
+++ MediSearch/src/app/api/zones/[pharmacy]/route.ts
@@ -1,7 +1,12 @@
 // Archivo: src/app/api/zones/[pharmacy]/route.ts
 import { NextRequest, NextResponse } from 'next/server';
+import type { NextApiRequest } from 'next';
+import type { Params } from 'next/dist/shared/lib/router/utils/route-matcher'; // ✨ tipo correcto
 
-export async function GET(_req: NextRequest, context: { params: { pharmacy: string } }) {
+export async function GET(
+  _req: NextRequest,
+  context: { params: Params } // ✅ este es el tipo correcto
+) {
   try {
     const { pharmacy } = context.params;
     const slug = pharmacy.toLowerCase().replace(/\s/g, '');
