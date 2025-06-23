3957a4455006a8b6d754bea67019de01ffa04c13
Tomás Poblete R <177884212+tomaspobb@users.noreply.github.com>
2025-06-22 23:56:38 -0400

    .

--- MediSearch/src/app/api/zones/[pharmacy]/route.ts
+++ MediSearch/src/app/api/zones/[pharmacy]/route.ts
@@ -1,15 +1,12 @@
 // Archivo: src/app/api/zones/[pharmacy]/route.ts
 import { NextRequest, NextResponse } from 'next/server';
-import type { NextApiRequest } from 'next';
-import type { Params } from 'next/dist/shared/lib/router/utils/route-matcher'; // ✨ tipo correcto
 
 export async function GET(
   _req: NextRequest,
-  context: { params: Params } // ✅ este es el tipo correcto
+  { params }: { params: { pharmacy: string } }
 ) {
   try {
-    const { pharmacy } = context.params;
-    const slug = pharmacy.toLowerCase().replace(/\s/g, '');
+    const slug = params.pharmacy.toLowerCase().replace(/\s/g, '');
 
     const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
     const url = `${baseUrl}/stock/zones/${slug}_stock_locations.json`;
